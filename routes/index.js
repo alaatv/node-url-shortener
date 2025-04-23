52243e1a6e142f5b01a66ecb09c04f5bd125a9e1
Shayan Yousefi <yusefshayan@gmail.com>
2025-04-23 13:19:30 +0330

    make healthcheck highest priority

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ module.exports = function (app, nus) {
   // api routes
   app.use('/api/v1', api);
 
+  // healthcheck route
+  app.get('/healthcheck', function (req, res) {
+    res.status(200).json({ status: 'OK' });
+  });
   // index route
   app.route('/').all(function (req, res) {
         res.redirect(301,'https://alaatv.com');
@@ -44,11 +48,7 @@ module.exports = function (app, nus) {
       }
     }, true);
   });
-    
-  // healthcheck route
-  app.get('/healthcheck', function (req, res) {
-    res.status(200).json({ status: 'OK' });
-  });
+
     
   // catch 404 and forwarding to error handler
   app.use(function(req, res, next) {
